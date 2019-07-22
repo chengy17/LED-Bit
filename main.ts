e5aa047bb6ca3e20c2a83fba96dfbb49730dd6b7
chengy17 <chengy_95@163.com>
2019-07-22 15:29:15 +0800

    del w add x

--- main.ts
+++ main.ts
@@ -229,8 +229,8 @@ namespace LEDBit {
         character_FACE20,
         
         
-        //% blockId="character_FACE23" block="char_W"
-        character_FACE23,
+         //% blockId="character_FACE24" block="char_X"
+         character_FACE24,
         
     }
 
@@ -255,8 +255,10 @@ namespace LEDBit {
     let S_show = pins.createBuffer(17);
     let T_show = pins.createBuffer(17);
   
+
+
+    let X_show = pins.createBuffer(17);
     
-    let W_show = pins.createBuffer(17);
     
 
     const A1_show: number[] = [0x0, 0x1, 0x0, 0x2, 0x80, 0x4, 0x40, 0x8, 0x20, 0x1f, 0xf0, 0x20, 0x8, 0x40, 0x4, 0x0, 0x0];
@@ -281,7 +283,8 @@ namespace LEDBit {
     const T1_show: number[] = [0x0, 0xf, 0xe0, 0x1, 0x0, 0x1, 0x0, 0x1, 0x0, 0x1, 0x0, 0x1, 0x0, 0x1, 0x0, 0x1, 0x0];
     
    
-    const W1_show: number[] = [0x0, 0x0, 0x0, 0x0, 0x0, 0x41, 0x4, 0x22, 0x88, 0x14, 0x50, 0x8, 0x20, 0x0, 0x0, 0x0, 0x0];
+    
+    const X1_show: number[] = [0x0, 0x0, 0x0, 0x8, 0x20, 0x4, 0x40, 0x2, 0x80, 0x1, 0x0, 0x2, 0x80, 0x4, 0x40, 0x8, 0x20];
 
    
 
@@ -739,13 +742,13 @@ namespace LEDBit {
             }
                 
             
-            case characterExpression.character_FACE23: {
-                W_show[0] = W1_show[0];
+            case characterExpression.character_FACE24: {
+                X_show[0] = X1_show[0];
                 for (let i = 1; i < 17; i += 2) {
-                    W_show[i] = W1_show[i + 1];
-                    W_show[i + 1] = W1_show[i];
+                    X_show[i] = X1_show[i + 1];
+                    X_show[i + 1] = X1_show[i];
                 }
-                pins.i2cWriteBuffer(HT16K33_ADDRESS, W_show);
+                pins.i2cWriteBuffer(HT16K33_ADDRESS, X_show);
                 break;
             }
             
